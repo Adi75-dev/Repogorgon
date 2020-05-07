1573e23ef229fe1e49ef744e7939c0a10a8e0ddb
Adi75-dev <adityamhambrey@gmail.com>
2020-05-07 22:34:02 +0530

    Functions Program

--- Python/01Intermediate/functions.py
+++ Python/01Intermediate/functions.py
@@ -1,5 +1,29 @@
-def increment(number, by):
-    return (number, number+by)
+# def fib(n):
+#     a, b = 0, 1
+#     while a < n:
+#         print(a, end=' ')
+#         a, b = b, a+b
+#     print()
 
 
-print(increment(2, 3))
+# fib(2000)
+
+
+nterms = int(input("How many terms? "))
+
+n1, n2, count = 0, 1, 0
+
+if nterms <= 0:
+    print('Enter positive integer')
+elif nterms == 1:
+    print("Fibonnaci sequence upto", nterms, ":")
+    print(n1)
+else:
+    print("Fibonacci sequence:")
+    while count < nterms:
+        print(n1)
+        nth = n1 + n2
+        # update values
+        n1 = n2
+        n2 = nth
+        count += 1
--- Python/02Basics/app.py
+++ Python/02Basics/app.py
@@ -1,3 +1,4 @@
+# To check whether a letter is present in the list
 names = ["John", "Mary"]
 for name in names:
     if name.startswith("J"):
@@ -5,3 +6,38 @@ for name in names:
         break
 else:
     print('Not found')
+
+
+x = int(input("Please Enter an Integer: "))
+
+if x < 0:
+    x = 0
+    print('Negative changed to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
+
+
+# Measure Strings
+words = ['Cat', 'window', 'defenestrate']
+for w in words:
+    print(w, len(w))
+
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        print(n, 'is a prime number')
+
+
+for num in range(2, 10):
+    if num % 2 == 0:
+        print("Found an even number", num)
+    else:
+        print("Found a number", num)
